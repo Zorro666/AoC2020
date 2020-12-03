9ea08d866d89d8fc4a611c207359e88c8f2d19c8
Jake Turner <jake@unity3d.com>
2020-12-03 06:44:12 +0000

    Day3 Part1 done, start of part2

--- AoC2020/Day03/Day03.cs
+++ AoC2020/Day03/Day03.cs
@@ -60,12 +60,34 @@ In this example, traversing the map using this slope would cause you to encounte
 
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 
+Your puzzle answer was 153.
+
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
 */
 
 namespace Day03
 {
     class Program
     {
+        const int MAX_WIDTH = 32;
+        const int MAX_HEIGHT = 512;
+        static readonly byte[,] sTrees = new byte[MAX_HEIGHT, MAX_WIDTH];
+        static int sHeight;
+        static int sWidth;
         private Program(string inputFile, bool part1)
         {
             var lines = AoC.Program.ReadLines(inputFile);
@@ -75,7 +97,7 @@ namespace Day03
             {
                 var result1 = Part1(3, 1);
                 Console.WriteLine($"Day03 : Result1 {result1}");
-                var expected = -123;
+                var expected = 153;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -95,12 +117,40 @@ namespace Day03
 
         public static void Parse(string[] lines)
         {
-            throw new NotImplementedException();
+            var height = lines.Length;
+            if (height > MAX_HEIGHT)
+            {
+                throw new InvalidProgramException($"height > MAX_HEIGHT {height} > {MAX_HEIGHT}");
+            }
+            var width0 = lines[0].Length;
+            for (var y = 0; y < height; ++y)
+            {
+                var line = lines[y];
+                var width = line.Length;
+                if (width != width0)
+                {
+                    throw new InvalidProgramException($"Inconsistent width {width} != {width0}");
+                }
+                for (var x = 0; x < width; ++x)
+                {
+                    sTrees[y, x] = line[x] == '#' ? 1 : 0;
+                }
+            }
+            sHeight = height;
+            sWidth = width0;
         }
 
         public static int Part1(int dx, int dy)
         {
-            throw new NotImplementedException();
+            var count = 0;
+            var x = 0;
+            for (var y = 1; y < sHeight; ++y)
+            {
+                x += 3;
+                x %= sWidth;
+                count += sTrees[y, x];
+            }
+            return count;
         }
 
         public static void Run()
