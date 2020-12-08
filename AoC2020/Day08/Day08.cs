22f56d53b4d47faf75396a3782000aa6deaed148
Jake Turner <jake@unity3d.com>
2020-12-08 05:45:08 +0000

    Day8 Part1 & Part2 done

--- AoC2020/Day08/Day08.cs
+++ AoC2020/Day08/Day08.cs
@@ -2,6 +2,110 @@
 
 /*
 
+--- Day 8: Handheld Halting ---
+
+Your flight to the major airline hub reaches cruising altitude without incident.
+While you consider checking the in-flight menu for one of those drinks that come with a little umbrella, you are interrupted by the kid sitting next to you.
+
+Their handheld game console won't turn on! They ask if you can take a look.
+
+You narrow the problem down to a strange infinite loop in the boot code (your puzzle input) of the device.
+You should be able to fix it, but first you need to be able to run the code in isolation.
+
+The boot code is represented as a text file with one instruction per line of text.
+Each instruction consists of an operation (acc, jmp, or nop) and an argument (a signed number like +4 or -20).
+
+acc increases or decreases a single global value called the accumulator by the value given in the argument.
+For example, acc +7 would increase the accumulator by 7.
+The accumulator starts at 0.
+After an acc instruction, the instruction immediately below it is executed next.
+jmp jumps to a new instruction relative to itself.
+The next instruction to execute is found using the argument as an offset from the jmp instruction; for example, jmp +2 would skip the next instruction, jmp +1 would continue to the instruction immediately below it, and jmp -20 would cause the instruction 20 lines above to be executed next.
+nop stands for No OPeration - it does nothing.
+The instruction immediately below it is executed next.
+For example, consider the following program:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+These instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2, 8(!)
+jmp +4  | 3
+acc +3  | 6
+jmp -3  | 7
+acc -99 |
+acc +1  | 4
+jmp -4  | 5
+acc +6  |
+
+First, the nop +0 does nothing.
+Then, the accumulator is increased from 0 to 1 (acc +1) and jmp +4 sets the next instruction to the other acc +1 near the bottom.
+After it increases the accumulator from 1 to 2, jmp -4 executes, setting the next instruction to the only acc +3.
+It sets the accumulator to 5, and jmp -3 causes the program to continue back at the first acc +1.
+
+This is an infinite loop: with this sequence of jumps, the program will run forever.
+The moment the program tries to run any instruction a second time, you know it will never terminate.
+
+Immediately before the program would run an instruction a second time, the value in the accumulator is 5.
+
+Run your copy of the boot code.
+
+Immediately before any instruction is executed a second time, what value is in the accumulator?
+
+Your puzzle answer was 1331.
+
+--- Part Two ---
+
+After some careful analysis, you believe that exactly one instruction is corrupted.
+
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp.
+(No acc instructions were harmed in the corruption of this boot code.)
+
+The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file.
+By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+
+For example, consider the same program from above:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+
+If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never leaving that instruction.
+If you change almost any of the jmp instructions, the program will still eventually find another jmp instruction and loop forever.
+
+However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+
+After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last instruction in the file.
+With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp).
+
+What is the value of the accumulator after the program terminates?
+
 */
 
 namespace Day08
@@ -14,9 +118,9 @@ namespace Day08
 
             if (part1)
             {
-                var result1 = -666;
+                var result1 = Part1(lines);
                 Console.WriteLine($"Day08 : Result1 {result1}");
-                var expected = -123;
+                var expected = 1331;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -24,13 +128,122 @@ namespace Day08
             }
             else
             {
-                var result2 = -666;
+                var result2 = Part2(lines);
                 Console.WriteLine($"Day08 : Result2 {result2}");
-                var expected = -123;
+                var expected = 1121;
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
                 }
+            }
+        }
+
+        private static int RunProgram(string[] lines, out int pc)
+        {
+            var acc = 0;
+            pc = 0;
+            var instructionsCount = lines.Length;
+            var visited = new bool[instructionsCount];
+            while (!visited[pc])
+            {
+                visited[pc] = true;
+                var l = lines[pc].Trim();
+                var tokens = l.Split();
+                var instruction = tokens[0].Trim();
+                var value = int.Parse(tokens[1]);
+                if (instruction == "nop")
+                {
+                    ++pc;
+                }
+                else if (instruction == "acc")
+                {
+                    acc += value;
+                    ++pc;
+                }
+                else if (instruction == "jmp")
+                {
+                    pc += value;
+                }
+                else
+                {
+                    throw new InvalidProgramException($"Unknown instruction {instruction} '{l}'");
+                }
+                if (pc == instructionsCount)
+                {
+                    return acc;
+                }
+                if ((pc < 0) || (pc > instructionsCount))
+                {
+                    return int.MinValue;
+                }
+            }
+            return acc;
+        }
+
+        public static int Part1(string[] lines)
+        {
+            var instructionsCount = lines.Length;
+            var acc = RunProgram(lines, out int pc);
+            if ((pc < 0) || (pc >= instructionsCount))
+            {
+                throw new InvalidProgramException($"Invalid PC for Part1 {pc} {instructionsCount}");
+            }
+            return acc;
+        }
+
+        public static int Part2(string[] lines)
+        {
+            var instructionsCount = lines.Length;
+            var tried = new bool[instructionsCount];
+            for (var i = 0; i < instructionsCount; ++i)
+            {
+                var instruction = lines[i].Trim().Split()[0].Trim();
+                if (instruction == "nop")
+                {
+                    tried[i] = false;
+                }
+                else if (instruction == "acc")
+                {
+                    tried[i] = true;
+                }
+                else if (instruction == "jmp")
+                {
+                    tried[i] = false;
+                }
+            }
+            var nextInstructionToChange = 0;
+            while (true)
+            {
+                for (var i = nextInstructionToChange; i < instructionsCount; ++i)
+                {
+                    if (tried[i] == false)
+                    {
+                        tried[i] = true;
+                        nextInstructionToChange = i;
+                        break;
+                    }
+                }
+                var instruction = lines[nextInstructionToChange].Trim().Split()[0].Trim();
+                var savedInstruction = lines[nextInstructionToChange];
+                if (instruction == "nop")
+                {
+                    lines[nextInstructionToChange] = lines[nextInstructionToChange].Replace("nop", "jmp");
+                }
+                else if (instruction == "jmp")
+                {
+                    lines[nextInstructionToChange] = lines[nextInstructionToChange].Replace("jmp", "nop");
+                }
+                else
+                {
+                    throw new InvalidProgramException($"Unknown instruction {instruction} {nextInstructionToChange} MAX: {instructionsCount}");
+                }
+                var acc = RunProgram(lines, out int pc);
+                if (pc == instructionsCount)
+                {
+                    return acc;
+                }
+                lines[nextInstructionToChange] = savedInstruction;
+                ++nextInstructionToChange;
             }
         }
 
--- AoC2020/Day08/Day08Tests.cs
+++ AoC2020/Day08/Day08Tests.cs
@@ -5,11 +5,37 @@ namespace Day08
     public class Tests
     {
         [Theory]
-        [InlineData(10)]
-        public void Day08(int expected)
+        [InlineData(new string[] {
+"nop +0",
+"acc +1",
+"jmp +4",
+"acc +3",
+"jmp -3",
+"acc -99",
+"acc +1",
+"jmp -4",
+"acc +6"
+        }, 5)]
+        public void Part1(string[] lines, int expected)
         {
-            Assert.NotEqual(expected, expected);
+            Assert.Equal(expected, Program.Part1(lines));
+        }
 
+        [Theory]
+        [InlineData(new string[] {
+"nop +0",
+"acc +1",
+"jmp +4",
+"acc +3",
+"jmp -3",
+"acc -99",
+"acc +1",
+"jmp -4",
+"acc +6"
+        }, 8)]
+        public void Part2(string[] lines, int expected)
+        {
+            Assert.Equal(expected, Program.Part2(lines));
         }
     }
 }
