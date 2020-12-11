3ba1e01aa64f8034cfbef52171d93de1c8331769
Jake Turner <jake@unity3d.com>
2020-12-11 19:21:16 +0000

    Day11 Part1 Start

--- AoC2020/Day11/Day11Tests.cs
+++ AoC2020/Day11/Day11Tests.cs
@@ -5,11 +5,39 @@ namespace Day11
     public class Tests
     {
         [Theory]
-        [InlineData(10)]
-        public void Day11(int expected)
+        [InlineData(new string[] {
+"L.LL.LL.LL",
+"LLLLLLL.LL",
+"L.L.L..L..",
+"LLLL.LL.LL",
+"L.LL.LL.LL",
+"L.LLLLL.LL",
+"..L.L.....",
+"LLLLLLLLLL",
+"L.LLLLLL.L",
+"L.LLLLL.LL"
+        }, 37)]
+        public void Part1(string[] lines, int expected)
         {
-            Assert.NotEqual(expected, expected);
+            Assert.Equal(expected, Program.Part1(lines));
+        }
 
+        [Theory]
+        [InlineData(new string[] {
+"L.LL.LL.LL",
+"LLLLLLL.LL",
+"L.L.L..L..",
+"LLLL.LL.LL",
+"L.LL.LL.LL",
+"L.LLLLL.LL",
+"..L.L.....",
+"LLLLLLLLLL",
+"L.LLLLLL.L",
+"L.LLLLL.LL"
+        }, 37)]
+        public void Part2(string[] lines, int expected)
+        {
+            Assert.Equal(expected, Program.Part2(lines));
         }
     }
 }
