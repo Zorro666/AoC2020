3ac3abefe8de5a800a50bfda6b05e3207d043405
Jake Turner <jake@unity3d.com>
2020-12-12 06:16:06 +0000

    Day12 Part2 done

--- AoC2020/Day12/Day12.cs
+++ AoC2020/Day12/Day12.cs
@@ -44,6 +44,43 @@ Figure out where the navigation instructions lead.
 
 What is the Manhattan distance between that location and the ship's starting position?
 
+Your puzzle answer was 319.
+
+--- Part Two ---
+
+Before you can give the destination to the captain, you realize that the actual action meanings were printed on the back of the instructions the whole time.
+
+Almost all of the actions indicate how to move a waypoint which is relative to the ship's position:
+
+Action N means to move the waypoint north by the given value.
+Action S means to move the waypoint south by the given value.
+Action E means to move the waypoint east by the given value.
+Action W means to move the waypoint west by the given value.
+Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+Action F means to move forward to the waypoint a number of times equal to the given value.
+The waypoint starts 10 units east and 1 unit north relative to the ship.
+The waypoint is relative to the ship; that is, if the ship moves, the waypoint moves with it.
+
+For example, using the same instructions as above:
+
+F10 moves the ship to the waypoint 10 times (a total of 100 units east and 10 units north), leaving the ship at east 100, north 10.
+The waypoint stays 10 units east and 1 unit north of the ship.
+N3 moves the waypoint 3 units north to 10 units east and 4 units north of the ship.
+The ship remains at east 100, north 10.
+F7 moves the ship to the waypoint 7 times (a total of 70 units east and 28 units north), leaving the ship at east 170, north 38.
+The waypoint stays 10 units east and 4 units north of the ship.
+R90 rotates the waypoint around the ship clockwise 90 degrees, moving it to 4 units east and 10 units south of the ship.
+The ship remains at east 170, north 38.
+F11 moves the ship to the waypoint 11 times (a total of 44 units east and 110 units south), leaving the ship at east 214, south 72.
+The waypoint stays 4 units east and 10 units south of the ship.
+
+After these operations, the ship's Manhattan distance from its starting position is 214 + 72 = 286.
+
+Figure out where the navigation instructions actually lead.
+
+What is the Manhattan distance between that location and the ship's starting position?
+
 */
 
 namespace Day12
@@ -68,7 +105,7 @@ namespace Day12
             {
                 var result2 = Part2(lines);
                 Console.WriteLine($"Day12 : Result2 {result2}");
-                var expected = -123;
+                var expected = 50157;
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
@@ -184,7 +221,109 @@ namespace Day12
 
         public static int Part2(string[] lines)
         {
-            throw new NotImplementedException();
+            var x = 0;
+            var y = 0;
+
+            var wayDX = +10;
+            var wayDY = -1;
+
+            foreach (var l in lines)
+            {
+                var line = l.Trim();
+                var move = line[0];
+                var distance = int.Parse(line[1..]);
+                if (move == 'N')
+                {
+                    wayDY -= distance;
+                }
+                else if (move == 'S')
+                {
+                    wayDY += distance;
+                }
+                else if (move == 'E')
+                {
+                    wayDX += distance;
+                }
+                else if (move == 'W')
+                {
+                    wayDX -= distance;
+                }
+                else if (move == 'L')
+                {
+                    var newDX = wayDX;
+                    var newDY = wayDY;
+                    if (distance == 90)
+                    {
+                        //+0, -1 => -1, +0
+                        //-1, +0 => +0, +1
+                        //+0, +1 => +1, +0
+                        //+1, +0 => +0, -1
+                        newDX = +wayDY;
+                        newDY = -wayDX;
+                    }
+                    else if (distance == 180)
+                    {
+                        //+0, -1 => +0, +1
+                        //-1, +0 => +1, +0
+                        //+0, +1 => +0, -1
+                        //+1, +0 => -1, +0
+                        newDX = -wayDX;
+                        newDY = -wayDY;
+                    }
+                    else if (distance == 270)
+                    {
+                        //+0, -1 => +1, +0
+                        //-1, +0 => +0, -1
+                        //+0, +1 => -1, +0
+                        //+1, +0 => +0, +1
+                        newDX = -wayDY;
+                        newDY = +wayDX;
+                    }
+                    wayDX = newDX;
+                    wayDY = newDY;
+                }
+                else if (move == 'R')
+                {
+                    var newDX = wayDX;
+                    var newDY = wayDY;
+                    if (distance == 90)
+                    {
+                        //+0, -1 => +1, +0
+                        //-1, +0 => +0, -1
+                        //+0, +1 => -1, +0
+                        //+1, +0 => +0, +1
+                        newDX = -wayDY;
+                        newDY = +wayDX;
+                    }
+                    else if (distance == 180)
+                    {
+                        //+0, -1 => +0, +1
+                        //-1, +0 => +1, +0
+                        //+0, +1 => +0, -1
+                        //+1, +0 => -1, +0
+                        newDX = -wayDX;
+                        newDY = -wayDY;
+                    }
+                    else if (distance == 270)
+                    {
+                        //+0, -1 => -1, +0
+                        //-1, +0 => +0, +1
+                        //+0, +1 => +1, +0
+                        //+1, +0 => +0, -1
+                        newDX = +wayDY;
+                        newDY = -wayDX;
+                    }
+                    wayDX = newDX;
+                    wayDY = newDY;
+                }
+                else if (move == 'F')
+                {
+                    x += wayDX * distance;
+                    y += wayDY * distance;
+                }
+            }
+
+            return Math.Abs(x) + Math.Abs(y);
         }
 
         public static void Run()
--- AoC2020/Day12/Day12Tests.cs
+++ AoC2020/Day12/Day12Tests.cs
@@ -16,5 +16,18 @@ namespace Day12
         {
             Assert.Equal(expected, Program.Part1(lines));
         }
+
+        [Theory]
+        [InlineData(new string[] {
+"F10",
+"N3",
+"F7",
+"R90",
+"F11"
+        }, 286)]
+        public void Part2(string[] lines, int expected)
+        {
+            Assert.Equal(expected, Program.Part2(lines));
+        }
     }
 }
