183030d93c2ee7c8d0df0c7ce57c013ac48ed098
Jake Turner <jake@unity3d.com>
2020-12-13 20:32:58 +0000

    Day13 Part1 and Part2 done

--- AoC2020/Day13/Day13Tests.cs
+++ AoC2020/Day13/Day13Tests.cs
@@ -6,9 +6,9 @@ namespace Day13
     {
         [Theory]
         [InlineData(new string[] {
-"A",
-""
-        }, 10)]
+"939",
+"7,13,x,x,59,x,31,19"
+        }, 295)]
         public void Part1(string[] lines, int expected)
         {
             Assert.Equal(expected, Program.Part1(lines));
@@ -16,9 +16,29 @@ namespace Day13
 
         [Theory]
         [InlineData(new string[] {
-"A",
-""
-        }, 10)]
+"939",
+"7,13,x,x,59,x,31,19"
+        }, 1068781)]
+        [InlineData(new string[] {
+"939",
+"17,x,13,19"
+        }, 3417)]
+        [InlineData(new string[] {
+"939",
+"67,7,59,61"
+        }, 754018)]
+        [InlineData(new string[] {
+"939",
+"67,x,7,59,61"
+        }, 779210)]
+        [InlineData(new string[] {
+"939",
+"67,7,x,59,61"
+        }, 1261476)]
+        [InlineData(new string[] {
+"939",
+"1789,37,47,1889"
+        }, 1202161486)]
         public void Part2(string[] lines, int expected)
         {
             Assert.Equal(expected, Program.Part2(lines));
