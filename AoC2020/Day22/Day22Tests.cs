8578c8beb7393d9905564f1bfa08f8ffced7158d
Jake Turner <jake@unity3d.com>
2020-12-22 11:28:34 +0000

    Day22 Preparation

--- AoC2020/Day22/Day22Tests.cs
+++ AoC2020/Day22/Day22Tests.cs
@@ -5,10 +5,24 @@ namespace Day22
     public class Tests
     {
         [Theory]
-        [InlineData(10)]
-        public void Day22(int expected)
+        [InlineData(new string[] {
+"Player 1:",
+"9",
+"2",
+"6",
+"3",
+"1",
+"",
+"Player 2:",
+"5",
+"8",
+"4",
+"7",
+"10"
+        }, 306)]
+        public void Day22(string[] lines, int expected)
         {
-            Assert.NotEqual(expected, expected);
+            Assert.NotEqual(expected, Program.Part1(lines));
 
         }
     }
