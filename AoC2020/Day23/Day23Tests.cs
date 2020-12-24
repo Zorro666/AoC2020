7a55eaae5684cd05c6b02546bf1f80ecf314197b
Jake Turner <jake@unity3d.com>
2020-12-24 14:38:11 +0000

    Day23 Preparation

--- AoC2020/Day23/Day23Tests.cs
+++ AoC2020/Day23/Day23Tests.cs
@@ -5,11 +5,24 @@ namespace Day23
     public class Tests
     {
         [Theory]
-        [InlineData(10)]
-        public void Day23(int expected)
+        [InlineData(new string[] {
+        "389125467"
+            }, 10, 92658374L)]
+        [InlineData(new string[] {
+        "389125467"
+            }, 100, 67384529L)]
+        public void Part1(string[] lines, int moves, long expected)
         {
-            Assert.NotEqual(expected, expected);
+            Assert.Equal(expected, Program.Part1(lines, moves));
+        }
 
+        [Theory]
+        [InlineData(new string[] {
+        "389125467"
+            }, 100, 67384529L)]
+        public void Part2(string[] lines, int moves, long expected)
+        {
+            Assert.Equal(expected, Program.Part2(lines, moves));
         }
     }
 }
