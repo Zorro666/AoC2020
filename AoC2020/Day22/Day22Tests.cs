e5185fdb4f2e5354f05b27486497ae0700079975
Jake Turner <jake@unity3d.com>
2020-12-26 08:48:18 +0000

    Day22 Part2 preparation

--- AoC2020/Day22/Day22Tests.cs
+++ AoC2020/Day22/Day22Tests.cs
@@ -20,10 +20,30 @@ namespace Day22
 "7",
 "10"
         }, 306)]
-        public void Day22(string[] lines, int expected)
+        public void Part1(string[] lines, int expected)
         {
             Assert.Equal(expected, Program.Part1(lines));
+        }
 
+        [Theory]
+        [InlineData(new string[] {
+"Player 1:",
+"9",
+"2",
+"6",
+"3",
+"1",
+"",
+"Player 2:",
+"5",
+"8",
+"4",
+"7",
+"10"
+        }, 291)]
+        public void Part2(string[] lines, int expected)
+        {
+            Assert.Equal(expected, Program.Part2(lines));
         }
     }
 }
